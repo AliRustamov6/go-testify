f0dea5d73b5935c9f6b1403931676855963a7be2
ALIRUSTAMOV <zirr666@bk.ru>
2024-09-23 21:27:45 +0300

    second iteration

--- precode.go
+++ precode.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"net/http"
-	"net/http/httptest"
 	"strconv"
 	"strings"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 var cafeList = map[string][]string{
@@ -47,58 +42,4 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(answer))
-}
-
-func TestMainHandlerCorrectRequest(t *testing.T) {
-
-	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil) // здесь создан запрос к сервису
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	// проверка статуса ответа
-	require.Equal(t, http.StatusOK, responseRecorder.Code)
-
-	// проверка тело ответа
-	body := responseRecorder.Body.String()
-	assert.NotEmpty(t, body) // убедимся что тело не пустое
-
-	cafe := strings.Split(body, ",")
-	assert.Len(t, cafe, 2) // ожидаем 2 кафе
-}
-
-func TestMainHandlerInvalidCity(t *testing.T) {
-
-	req := httptest.NewRequest("GET", "/cafe?count=3&city=krasnodar", nil) // здесь создан запрос к сервису
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	// проверка статуса ответа
-	require.Equal(t, http.StatusBadRequest, responseRecorder.Code)
-
-	// проверка тело ответа
-	body := responseRecorder.Body.String()
-	assert.Equal(t, "wrong city value", body) // оджидается сообшение об ошибке
-}
-
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil) // здесь создан запрос к сервису
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	// проверка статуса ответа
-	require.Equal(t, http.StatusOK, responseRecorder.Code)
-
-	// проверка тело ответа
-	body := responseRecorder.Body.String()
-	assert.NotEmpty(t, body) // убедимся что тело не пустое
-
-	cafe := strings.Split(body, ",")
-	assert.Len(t, cafe, totalCount) // ожидаем 4 кафе
 }
